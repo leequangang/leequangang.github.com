963626b9aa622a9df6f2d115c86bc4191340d83d
leequangang <leequangang@gmail.com>
2015-06-16 01:36:11 +0800

    更新js

--- local/src/css/classie.js
+++ local/src/css/classie.js
@@ -1,6 +1,8 @@
 /*!
- * classie - class helper functions
+ * classie v1.0.1
+ * class helper functions
  * from bonzo https://github.com/ded/bonzo
+ * MIT license
  * 
  * classie.has( elem, 'my-class' ) -> true/false
  * classie.add( elem, 'my-new-class' )
@@ -8,7 +10,8 @@
  * classie.toggle( elem, 'my-class' )
  */
 
-/*jshint browser: true, strict: true, undef: true */
+/*jshint browser: true, strict: true, undef: true, unused: true */
+/*global define: false, module: false */
 
 ( function( window ) {
 
@@ -54,7 +57,7 @@ function toggleClass( elem, c ) {
   fn( elem, c );
 }
 
-window.classie = {
+var classie = {
   // full names
   hasClass: hasClass,
   addClass: addClass,
@@ -66,5 +69,17 @@ window.classie = {
   remove: removeClass,
   toggle: toggleClass
 };
+
+// transport
+if ( typeof define === 'function' && define.amd ) {
+  // AMD
+  define( classie );
+} else if ( typeof exports === 'object' ) {
+  // CommonJS
+  module.exports = classie;
+} else {
+  // browser global
+  window.classie = classie;
+}
 
 })( window );
